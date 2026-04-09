6c97c85526d893b43d3ea808f195204d4831acf4
Dangana Joshua <joshuatinoo33@gmail.com>
2026-04-09 20:45:14 +0100

    removed code line

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express =  require('express');
 const cors = require('cors');
+const path = require("path");
 const reportController = require('./controllers/reportController');
 const registerController = require('./controllers/registerController');
 const loginController = require('./controllers/loginController');
