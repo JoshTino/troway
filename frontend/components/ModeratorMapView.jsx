f8145d9fb5c07830e2bbb5468274e31a5f132043
Dangana Joshua <joshuatinoo33@gmail.com>
2026-04-14 15:14:19 +0100

    feat: added moderator map view

---
+++ frontend/components/ModeratorMapView.jsx
@@ -0,0 +1,55 @@
+import {useState, useEffect} from "react"
+import {useNavigate} from "react-router-dom"
+import ModeratorClusterMap from "/components/ModeratorClusterMap"
+import BASE_URL from "/constants/base-url"
+import AdminNavigation from "/components/AdminNavigation"
+
+const ModeratorMapView = () => {
+	const navigate = useNavigate();
+	const token = localStorage.getItem("token");
+	const [reports, setReport] = useState([]);
+
+	console.log(localStorage.getItem("token", "id"));
+
+	useEffect( () => {
+		fetch(`${BASE_URL}/get_moderator_task`, {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		})
+		.then(res => res.json())
+		.then(data => {
+			setReport(data);
+		})
+		.catch(err => console.log(err));
+	}, []);
+
+	return(
+		<>	
+			{/*<nav className="flex justify-center">
+				<div className="flex w-11/12 justify-between rounded-lg mt-3 bg-white p-4">
+					<h1 className="text-4xl text-center text-green-400 font-nunito font-bold">TroWay</h1>
+					<button onClick={logOut} className="bg-red-500 text-white cursor-pointer rounded-sm px-4 py-2 font-light font-nunito">Logout</button>
+				</div>
+			</nav>*/}
+			<AdminNavigation navigate={navigate} />
+			<div className="flex justify-center">
+				<div className="bg-white w-11/12 rounded-lg mt-3 p-1 shadow-lg">
+						<div className="w-full">
+							<div className="w-full">
+								{reports && reports.length > 0 ? (
+
+									<ModeratorClusterMap reports={reports} /*moderators={moderators} selectedModerator={selectedModerator} handleModeratorSelect={handleModeratorSelect} submitModeratorTask={submitModeratorTask} showSuccessModal={showSuccessModal} modalMessage={modalMessage}*//>
+								) : (
+									<p>Map loading...</p>
+								)}
+							</div>
+						</div>
+				</div>
+			</div>
+		</>
+	);
+
+}
+export default ModeratorMapView;
