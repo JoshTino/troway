7c3315a57070fe293bad75a3edb3fff0525f18f9
Dangana Joshua <joshuatinoo33@gmail.com>
2026-04-17 23:40:18 +0100

    feat: changed route logic, to mark completed task

--- backend/controllers/moderatorController.js
+++ backend/controllers/moderatorController.js
@@ -6,7 +6,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 
 module.exports = (app) => {
-	app.get("/get_moderator_task", authMiddleware, async (req, res) => {
+	app.get('/get_moderator_task', authMiddleware, async (req, res) => {
 		const moderatorId =  req.user.id;
 
 		try {
@@ -20,13 +20,18 @@ module.exports = (app) => {
 		}
 	});
 
-	app.post('/mark_completed_task', authMiddleware, multer.upload.single("file"), async (req, res) => {
+	app.post('/mark_completed_task/:id', authMiddleware, multer.upload.single("file"), async (req, res) => {
+
+		const reportId = req.params.id;
 		const moderatorId = req.user.id;
 		const file = req.file.filename;
 		console.log(file);
 
 		try {
-			const taskCompleted = await Report.findByIdAndUpdate(moderatorId, {completedBy: moderatorId, photoEvidence: file});
+			const taskCompleted = await Report.findOneAndUpdate(
+				{_id: reportId}, 
+				{ $set: {status: "cleared", completedBy: moderatorId, photoEvidence: file} },
+				{returnDocument: "after"} );
 
 			if (taskCompleted) return res.status.json(taskCompleted);
 
