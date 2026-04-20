3142c63ddad486ad5a950f60dcefcb8e89b0451c
Dangana Joshua <joshuatinoo33@gmail.com>
2026-04-20 18:59:07 +0100

    chore: removed sensitive comment

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 
-/*mongoose.connect('mongodb+srv://test:test@user.gadx4gh.mongodb.net/?appName=user')
-.then(() => console.log("MongoDB Connected"));*/
-
-
 const userSchema = new mongoose.Schema({
 	name: String,
 	email: String,
