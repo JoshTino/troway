6d7efed5573b91ab984f69d18cde2125a84e3bb4
Dangana Joshua <joshuatinoo33@gmail.com>
2026-04-22 11:15:14 +0100

    feat: add marker to get truck(s) position on map

---
+++ frontend/components/MultiTruckMarker.jsx
@@ -0,0 +1,34 @@
+import {Marker, useMap} from "react-leaflet"
+import {useEffect, useRef} from "react"
+import "leaflet/dist/leaflet.css"
+import L from "leaflet"
+
+
+const TruckMarker = ( {location} ) => {
+	const markerRef = useRef();
+	const map = useMap();
+
+	const truckIcon = new L.Icon({
+		iconUrl: "/icons/t1.png",
+		iconSize: [65, 65]
+	});
+	// if (!location) return null;
+
+	useEffect( () => {
+		//if (!location || !markerRef.current) return;
+
+		markerRef.current.setLatLng([location.lat, location.lng]);
+		//map.panTo([location.lat, location.lng]);
+
+	}, [location]);
+
+	return (
+		
+			<Marker 
+				ref={markerRef}
+				position={[location.lat, location.lng]}
+				icon={truckIcon}
+			/>
+	);
+}
+export default TruckMarker;
