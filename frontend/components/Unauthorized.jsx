8e543046a09551ded4a6c8a4e9b00a5dcbb0c2ad
Dangana Joshua <joshuatinoo33@gmail.com>
2026-04-30 04:51:42 +0100

    feat: add fallback page for unauthorized access

---
+++ frontend/components/Unauthorized.jsx
@@ -0,0 +1,11 @@
+
+const Unauthorized = () => {
+	return (
+		<>
+			<div className="bg-gray-200 flex justify-center items-center">
+				<h1 className="text-4xl w-11/12 p-4 font-bold font-nunito">Ohh Snap! Unauthorized Access</h1>
+			</div>
+		</>
+	);
+}
+export default Unauthorized;
