0a1039d3802da216d07cb8b34035eaa7479955f9
Dangana Joshua <joshuatinoo33@gmail.com>
2026-05-01 03:12:52 +0100

    feat: show hamburger option at medium screen and add hover effect

--- frontend/components/HamburgerNav.jsx
+++ frontend/components/HamburgerNav.jsx
@@ -17,14 +17,14 @@ const HamburgerNav = () => {
 		<nav className="flex flex-col items-center justify-center">
 				<div className="flex w-11/12 justify-between rounded-lg mt-3 bg-white p-4">
 					<h1 className="text-4xl text-center text-green-400 font-nunito font-bold">TroWay</h1>
-					<button className="text-gray-700" onClick={() => setOpen(!open)}>
+					<button className="text-gray-700 cursor-pointer " onClick={() => setOpen(!open)}>
 						{open ? <X size={40}/> : <Menu size={40} />}
 					</button>					
 				</div>
 				{open && (
-					<div className="bg-white w-5/12 flex flex-col self-end mr-5 absolute top-21 border border-1 border-gray-200 md:hidden">
-						<button onClick={() => navigate('/profile')} className="text-black cursor-pointer rounded-sm py-2 font-light font-nunito">Profile</button>
-						<button onClick={logOut} className="text-black cursor-pointer rounded-sm py-2 font-light font-nunito">Logout</button>
+					<div className="bg-white w-5/12 md:w-2/12 flex flex-col self-end mr-5 md:mr-15 absolute top-21 border border-1 border-gray-200">
+						<button onClick={() => navigate('/profile')} className="text-black hover:bg-gray-100 cursor-pointer rounded-sm py-2 font-light font-nunito">Profile</button>
+						<button onClick={logOut} className="text-black hover:bg-gray-100 cursor-pointer rounded-sm py-2 font-light font-nunito">Logout</button>
 					</div>
 				)}
 		</nav>
