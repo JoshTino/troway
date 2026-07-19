f837c1c7b23293a6ccdae5547e025508b38a904e
Dangana Joshua <joshuatinoo33@gmail.com>
2026-07-19 02:49:54 +0100

    feat: give user access to page view-user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
         />
 
         <Route path="/view-users" element={
-          <ProtectedRoute allowedRoles={["admin, moderator"]}>
+          <ProtectedRoute allowedRoles={["admin, moderator, user"]}>
             <AdminViewUsers />
           </ProtectedRoute>
         }/>
