8dff5f8cde3229923798c122a2824f3d18811ae1
Dangana Joshua <joshuatinoo33@gmail.com>
2026-08-29 12:58:44 +0100

    feat: Change navigate path & button span text

--- frontend/components/ModeratorNavigation.jsx
+++ frontend/components/ModeratorNavigation.jsx
@@ -6,9 +6,9 @@ const ModeratorNavigation = () => {
 		<>
 			<div className="flex justify-center">
 				<div className="w-11/12 p-2 rounded-lg mt-3 bg-white flex justify-center gap-x-4">
-					<span onClick={() => navigate("/waste-location")} className="text-xs bg-green-500/20 p-1 px-3 font-nunito cursor-pointer rounded-sm">View map</span>
-					<span onClick={() => navigate("/admin-metric")} className="text-xs bg-green-500/20 p-1 px-3 font-nunito cursor-pointer rounded-sm">Display metric</span>
-					<span onClick={() => navigate("/view-users")} className="text-xs bg-green-500/20 p-1 px-3 font-nunito cursor-pointer rounded-sm">View users</span>
+					<span onClick={() => navigate("/moderator-map-view")} className="text-xs bg-green-500/20 p-1 px-3 font-nunito cursor-pointer rounded-sm">View map</span>
+					<span onClick={() => navigate("/moderator-dashboard")} className="text-xs bg-green-500/20 p-1 px-3 font-nunito cursor-pointer rounded-sm">Dashboard</span>
+					{/*<span onClick={() => navigate("/view-users")} className="text-xs bg-green-500/20 p-1 px-3 font-nunito cursor-pointer rounded-sm">View users</span>*/}
 				</div>
 			</div>
 		</>
